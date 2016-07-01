bf2902157aff0e8dc43b02b1946253b2a2114231
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-01 21:35:03 +0700

    balik kembali ke awal

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,64 +1,49 @@
 <template>
-  <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
-    <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
-  </div>
+  <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" v-link="'home'">Belajar Vue JS</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a v-link="'home'">Home</a></li>
+            <li><a v-if="user" v-link="'barang'">Data Barang</a></li>
+            <li><a v-if="!user" v-link="'signIn'">Sign In</a></li>
+            <li><a v-if="!user" v-link="'signUp'">Sign Up</a></li>
+            <li><a v-if="user" @click="logout" href="#">Logout</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
+      <router-view></router-view>
+    </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import AuthenticationController from './authentication/authentication.controller';
 
 export default {
-  components: {
-    Hello
+  data() {
+    return {
+      user: AuthenticationController.user.authenticated
+    }
+  },
+  methods: {
+    logout() {
+      AuthenticationController.logout()
+      this.user = AuthenticationController.user.authenticated
+    }
+  },
+  ready() {
+    AuthenticationController.checkAuthenticate()
+    this.user = AuthenticationController.user.authenticated
   }
 }
 </script>
-
-<style>
-html {
-  height: 100%;
-}
-
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
-
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
-
-#app a {
-  color: #42b983;
-  text-decoration: none;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
-}
-</style>
